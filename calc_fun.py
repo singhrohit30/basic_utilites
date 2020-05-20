33d9f684a992c07342606f6d36cdd179ff5f01c4
Rohit <rhtkmr084@gmail.com>
2020-05-20 17:48:39 +0530

    Basic Calculator in Python

---
+++ calc_fun.py
@@ -0,0 +1,42 @@
+# Normal Calculator
+while True:
+    num1 = int(input("Enter first number :"))
+    num2 = int(input("Enter second number :"))
+    print("""
+Press + for Addition
+Press - for subtraction
+Press * for Multiplication
+Press / for Division
+""")
+
+    choice = input("Enter the operator: ")
+
+
+    def calc(digit1, digit2, operator):
+        if operator == "+":
+            result = digit1 + digit2
+            return f"The Sum is {result}"
+        elif operator == "-":
+            result = digit1 - digit2
+            return f"The Difference is {result}"
+        elif operator == "*":
+            result = digit1 * digit2
+            return f"The Product is {result}"
+        elif operator == "/":
+            result = digit1 // digit2
+            return f"The Division is {result}"
+        else:
+            result = "Enter correct choice "
+            return result
+
+
+    print(calc(num1, num2, choice))
+
+    again = input("Press 'Y' to Continue"
+                  "Press 'Q' to Quit").lower()
+    if again == 'y':
+        continue
+    else:
+        print("QUITING....THANKS FOR LETTING US SERVE YOU!!!!")
+        break
+
